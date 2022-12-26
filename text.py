6994d5607f114e303fd9010f7b7fa404c5f9b297
yoymmt <yoo@momo.so-net.ne.jp>
2022-12-26 19:37:51 +0900

    !*3

--- text.py
+++ text.py
@@ -1,5 +1,3 @@
-from datetime import date
-from socket import TCP_NODELAY
 from inky.auto import auto
 from PIL import Image, ImageFont, ImageDraw
 
@@ -10,10 +8,8 @@ img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
 from font_fredoka_one import FredokaOne
 
-font = ImageFont.truetype("/usr/share/fonts/truetype/fonts-japanese-gothic.ttf", 15, encoding='unic')
-
-  message = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd  =  'date "+%y/%m/%d(%a) %H:%M"'
+font = ImageFont.truetype(FredokaOne, 22)
+message = "Hello, World!!!"
 w, h = font.getsize(message)
 x = (inky_display.WIDTH / 2) - (w / 2)
 y = (inky_display.HEIGHT / 2) - (h / 2)
